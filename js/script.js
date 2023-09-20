478804e77443cbaaba26a5c5ec13b48bc9f5ff85
sergio alvarez pinto <sertech_live@hotmail.com>
2023-09-20 15:56:08 -0400

    142 details page backdrop

--- js/script.js
+++ js/script.js
@@ -84,6 +84,10 @@ async function displayMovieDetails() {
     console.log(movieId);
 
     const movie = await fetchAPIData(`movie/${movieId}`);
+
+    // Overlay for the background image
+    displayBackgroundImage('movie', movie.backdrop_path);
+
     const div = document.createElement('div');
     div.innerHTML = `
     <div class="details-top">
@@ -144,6 +148,28 @@ async function displayMovieDetails() {
     </div>
     `;
     document.querySelector('#movie-details').appendChild(div);
+}
+
+// display backdrop on details pages
+function displayBackgroundImage(type, backgroundPath) {
+    const overlayDiv = document.createElement('div');
+    overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
+    overlayDiv.style.backgroundSize = 'cover';
+    overlayDiv.style.backgroundPosition = 'center';
+    overlayDiv.style.backgroundRepeat = 'no-repeat';
+    overlayDiv.style.height = '100vh';
+    overlayDiv.style.width = '100vw';
+    overlayDiv.style.position = 'absolute';
+    overlayDiv.style.top = '0';
+    overlayDiv.style.left = '0';
+    overlayDiv.style.zIndex = '-1';
+    overlayDiv.style.opacity = '0.1';
+
+    if (type === 'movie') {
+        document.querySelector('#movie-details').appendChild(overlayDiv);
+    } else {
+        document.querySelector('#show-details').appendChild(overlayDiv);
+    }
 }
 
 // fetch data from TMDB API
