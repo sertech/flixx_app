1f5e2cf8583f7d2a017e5bc31a7c1bca722211a0
sergio alvarez pinto <sertech_live@hotmail.com>
2023-09-21 17:55:38 -0400

    147 add pagination for search FINISHED

--- js/script.js
+++ js/script.js
@@ -4,8 +4,10 @@ const global = {
     search: {
         term: '',
         type: '',
+        page: 1,
         pagination: 1,
         totalPages: 1,
+        totalResults: 0,
     },
     api: {
         apiKey: '0e97928887ec91c394e75fc9c3ddeb43',
@@ -269,8 +271,13 @@ async function search() {
 
     if (global.search.term !== '' && global.search.term !== null) {
         //todo - make request and display results
-        const { results, total_pages, page } = await searchAPIData();
+        const { results, total_pages, page, total_results } =
+            await searchAPIData();
         console.log(results);
+
+        global.search.page = page;
+        global.search.totalPages = total_pages;
+        global.search.totalResults = total_results;
 
         if (results.length === 0) {
             showAlert('No result found');
@@ -285,6 +292,11 @@ async function search() {
 }
 
 function displaySearchResults(results) {
+    // clear previous results
+    document.querySelector('#search-results').innerHTML = '';
+    document.querySelector('#search-results-heading').innerHTML = '';
+    document.querySelector('#pagination').innerHTML = '';
+
     results.forEach((result) => {
         const div = document.createElement('div');
         div.classList.add('card');
@@ -325,7 +337,50 @@ function displaySearchResults(results) {
             </p>
         </div>
         `;
+
+        document.querySelector('#search-results-heading').innerHTML = `
+            <h2>${results.length} of ${global.search.totalResults} results for ${global.search.term}</h2>
+        `;
+
         document.querySelector('#search-results').appendChild(div);
+    });
+    displayPagination();
+}
+
+// create & display pagination fro search
+function displayPagination() {
+    const div = document.createElement('div');
+    div.classList.add('pagination');
+    div.innerHTML = `
+        <button class="btn btn-primary" id="prev">Prev</button>
+        <button class="btn btn-primary" id="next">Next</button>
+        <div class="page-counter">Page ${global.search.page} of ${global.search.totalPages}</div>
+    `;
+
+    document.querySelector('#pagination').appendChild(div);
+
+    // disable prev button if on first page
+    if (global.search.page === 1) {
+        document.querySelector('#prev').disabled = true;
+    }
+
+    // disable next button if on last page
+    if (global.search.page === global.search.totalPages) {
+        document.querySelector('#next').disabled = true;
+    }
+
+    // next page
+    document.querySelector('#next').addEventListener('click', async () => {
+        global.search.page++;
+        const { results, total_pages } = await searchAPIData();
+        displaySearchResults(results);
+    });
+
+    // previous page
+    document.querySelector('#prev').addEventListener('click', async () => {
+        global.search.page--;
+        const { results, total_pages } = await searchAPIData();
+        displaySearchResults(results);
     });
 }
 
@@ -404,7 +459,7 @@ async function searchAPIData() {
     showSpinner();
 
     const response = await fetch(
-        `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`
+        `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}&page=${global.search.page}`
     );
     const data = await response.json();
 
